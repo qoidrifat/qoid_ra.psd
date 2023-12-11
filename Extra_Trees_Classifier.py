da60e6bf29b63e93a0dbcc3f3082405e2b6d9ceb
qoidrifat <125227459+qoidrifat@users.noreply.github.com>
2023-12-11 15:41:30 +0700

    Update Extra_Trees_Classifier.py
    Signed-off-by: qoidrifat <125227459+qoidrifat@users.noreply.github.com>

--- Extra_Trees_Classifier.py
+++ Extra_Trees_Classifier.py
@@ -1,12 +1,13 @@
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier  # Mengganti RandomForestClassifier dengan ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import streamlit as st
 
 # Ganti 'wine_dataset.csv' dengan nama file dataset yang sesuai
-url = "https://raw.githubusercontent.com/qoidrifat/psd.github.io/master/Wholesale%20customers%20data.csv"
-wc_data = pd.read_csv(url)
+# Pastikan file dataset sudah ada di direktori lokal atau gunakan URL yang valid
+file_path = "Wholesale customers data.csv"
+wc_data = pd.read_csv(file_path)
 
 # Pindahkan kolom 'Channel' ke paling kanan
 if 'Channel' in wc_data.columns:
@@ -21,7 +22,7 @@ y = wc_data['Channel']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Buat model Extra Trees Classifier
-et_model = ExtraTreesClassifier(n_estimators=100, random_state=42)  # Mengganti RandomForestClassifier dengan ExtraTreesClassifier
+et_model = ExtraTreesClassifier(n_estimators=100, random_state=42)
 
 # Latih model pada data pelatihan
 et_model.fit(X_train, y_train)
@@ -32,23 +33,24 @@ def predict_wc_channel(features):
     return prediction[0]
 
 # Judul aplikasi
-st.title("Aplikasi Untuk Mengklasifikasi Jumlah Channel Yang Digunakan pada Wholesale Customers Datasets")  # Mengganti judul aplikasi
+st.title("Aplikasi Untuk Memprediksi Jumlah Channel Yang Digunakan pada Wholesale Customers Datasets") 
 
 # Tampilkan akurasi model
 y_pred = et_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 st.write(f"Akurasi Model: {accuracy:.2f}")
 
-# Memungkinkan pengguna memasukkan semua fitur
-input_features = []
-for feature_name in wc_data.columns[:-1]:  # Menghindari kolom 'class'
-    value = st.slider(f"Masukkan nilai untuk {feature_name}", min_value=min(wc_data[feature_name]), max_value=max(wc_data[feature_name]))
-    input_features.append(value)
-
 # Tombol untuk melakukan prediksi
-if st.button("Prediksi Channel yang digunakan "):
-    if len(input_features) == len(wc_data.columns) - 1:  # Memastikan ada 13 nilai input
-        prediction = predict_wc_channel(input_features)
+input_features = {}
+for feature_name in wc_data.columns[:-1]:  
+    value = st.slider(f"Masukkan nilai untuk {feature_name}", 
+                      min_value=int(wc_data[feature_name].min()), 
+                      max_value=int(wc_data[feature_name].max()))
+    input_features[feature_name] = value
+
+if st.button("Prediksi Channel yang digunakan"):
+    if len(input_features) == len(wc_data.columns) - 1:  
+        prediction = predict_wc_channel(list(input_features.values()))
         st.write(f"Prediksi Channelnya adalah: {prediction}")
     else:
         st.write("Harap masukkan nilai untuk semua fitur")
