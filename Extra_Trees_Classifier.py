18562010aa8361560214a5cab445e0c04b2156e3
qoidrifat <125227459+qoidrifat@users.noreply.github.com>
2023-12-11 15:50:29 +0700

    Update Extra_Trees_Classifier.py
    Signed-off-by: qoidrifat <125227459+qoidrifat@users.noreply.github.com>

--- Extra_Trees_Classifier.py
+++ Extra_Trees_Classifier.py
@@ -21,7 +21,7 @@ y = wc_data['Channel']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Buat model Extra Trees Classifier
-et_model = ExtraTreesClassifier(n_estimators=100, random_state=42)
+et_model = (n_estimators=100, random_state=42)
 
 # Latih model pada data pelatihan
 et_model.fit(X_train, y_train)
