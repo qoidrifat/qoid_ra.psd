27fe1da1687f822dc16a1cad9d187718fd86dfd9
qoidrifat <125227459+qoidrifat@users.noreply.github.com>
2023-12-11 15:43:19 +0700

    Update Extra_Trees_Classifier.py
    Signed-off-by: qoidrifat <125227459+qoidrifat@users.noreply.github.com>

--- Extra_Trees_Classifier.py
+++ Extra_Trees_Classifier.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import streamlit as st
